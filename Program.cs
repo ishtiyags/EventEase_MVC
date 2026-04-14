3088dc770bab3264c5c7a1ed1e327d7082461f5d
ishtiyags <165825704+ishtiyags@users.noreply.github.com>
2026-04-14 17:36:07 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using EventEase.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Events}/{action=Index}/{id?}");
+
+app.Run();
